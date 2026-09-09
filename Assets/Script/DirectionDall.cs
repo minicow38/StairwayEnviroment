cf70a8e45796c6719fe023a91d1d0d73b816b3cb
kyuxin45 <r11.hello.world@wandit.jp>
2026-09-09 16:49:54 +0900

    padding

--- Assets/Script/DirectionDall.cs
+++ Assets/Script/DirectionDall.cs
@@ -9,28 +9,38 @@ using UnityEditor.Rendering;
 public class DirectionDall : MonoBehaviour
 {
     public string ContactNum = "";
-    private Collider[] aroundStairway;
-   private Collider[] AroundStairway;
+    public  Collider[] AroundStairway;
+    public List<GameObject> AroundStairwayRenderer;
+   
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    private void FixedUpdate()
+     void FixedUpdate()
     {
-        if (AroundStairway == null)
+        if (AroundStairway.Length == 0)
         {
             AroundStairway = Physics.OverlapSphere(transform.position, 5f);
+            for (int i = 0; i < AroundStairway.Length; i++)
+            {
+                if (Regex.Match(AroundStairway[i].name, @"^(\w*)(\d*)_(\d*)_(Physics)").Success)
+                {
+                    Match match = Regex.Match(AroundStairway[i].name, @"^(\w*)(\d*)_(\d*)_(Physics)");
+
+                    AroundStairwayRenderer.Add(AroundStairway[i].transform.gameObject);
+                    
+                }
+            }
+
+            Debug.Log("");
         }
     }
 
-    private void OnEnable()
-    {
-        Debug.Log("");
-    }
+   
     // Update is called once per frame
-    /*void OnCollisionEnter(Collision col)
+    void OnCollisionEnter(Collision col)
     {
         Match match = Regex.Match(col.transform.name, @"^(\w*)(\d*)_(\d*)_(Physics)");
         var AroundStairway = Physics.OverlapSphere(col.transform.position, 5f);
@@ -39,6 +49,8 @@ public class DirectionDall : MonoBehaviour
             if (AroundStairway[i].transform.name == col.transform.name)
             {
                var dir= AroundStairway[i].transform.position - AroundStairway[i - 1].transform.position;
+               var objName=match.Groups[1].Value + "_" + match.Groups[3].Value+"_"+ ("Render");
+
                Debug.Log("");
             }
         }
@@ -51,6 +63,6 @@ public class DirectionDall : MonoBehaviour
             int step = 0;
         }
         Debug.Log("");
-    }*/
+    }
     
 }
